fa204aba6c7e7041fafb4d4929897469f1fca91d
Jayant254 <73995288+Jayant254@users.noreply.github.com>
2020-11-30 16:53:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var a,b;
+
+
+function setup() {
+  createCanvas(800,400);
+  a=createSprite(400, 200, 50, 50);
+  b=createSprite(300, 200, 50, 50);
+  a.shapeColor=("blue");
+  b.shapeColor=("blue");
+}
+
+function draw() {
+  background(255,255,255);  
+  a.x=mouseX;
+  a.y=mouseY;
+ console.log(a.x-b.x);
+
+ if(a.x-b.x<=a.width/2+b.width/2&&b.x-a.x<=b.width/2+a.width/2
+  &&a.y-b.y<=a.height/2+b.height/2&&b.y-a.y<=b.height/2+a.height/2){
+  a.shapeColor=("red");
+  b.shapeColor=("red");
+ }
+ else{
+  a.shapeColor=("blue");
+  b.shapeColor=("blue");
+ }
+
+
+
+  drawSprites();
+}
